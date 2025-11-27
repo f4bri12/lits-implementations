c5f59186b188d1c5157daead28990ba880184460
f4bri12 <javinson@unicauca.edu.co>
2025-11-27 10:30:30 -0500

    Update arry list.h

--- arry list.h
+++ arry list.h
@@ -35,6 +35,47 @@
 		list -> element_size = element_size;
 }
 		void ArrayList_destroy(ArrayList*List){
-			if(List);
+			if(List) retun;
+			arrayList_clear(List);
+			free(List->data);
+			free(list);
 }
-
+			void ArrayList_add(ArrayList*List, void*element){
+				if (!list || !element) return;
+				if(list->size ->list->capacity){ 
+					arrayList_ensure_capacity(list->capacity* GROWTH_factor);
+			
+				}
+				list -> data [list->size] = malloc (list -> element_size);
+				if (!list->data[list->size]{
+					printf("Error al asignar memoria para el nuevo elemento,");
+					return;
+				}
+				memcpy(list_data[list->size], element, list->element_size);
+				list->size++;
+			}
+				void arrayList_insert(ArrayList*list, void *element, int index){
+					if(!list || !element || index < 0 || index > list-> size) return;
+					if (list -> size ->list capacity){
+						arrayList_ensure_capacity(list, list->capacity* GROWTH_factor);
+						
+					}
+				for (int i=list_size; i> index; i--){
+					list->data[i] = list->data[i- 1];
+				}
+				list->data [index] = malloc(list->element_size);
+				if (!list -> data [index]){
+					printf("Erro al solicitar memoria en la insercion"); return;
+				}
+				memcpy (list->data [index], element, list->element_size);
+				list->size++;
+				}
+			void arrayList_clear(ArrayList*List){
+				if(!list) return;
+				for (int i=0; i<list->size; i++){
+					free (list-> data[i]);
+					list->data[i]= NULL;
+					
+				}
+			list->size=0;
+			}
